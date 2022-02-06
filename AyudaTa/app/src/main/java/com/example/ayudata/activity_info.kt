3a7827c37243ad0e57f309583fd45646b7325ff3
Mau <mauriciotxai@gmail.com>
2022-02-05 18:44:17 -0600

    cambio de id de boton de info

--- AyudaTa/app/src/main/java/com/example/ayudata/activity_info.kt
+++ AyudaTa/app/src/main/java/com/example/ayudata/activity_info.kt
@@ -2,10 +2,14 @@ package com.example.ayudata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class activity_info : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        val botonReg:Button = findViewById(R.id.boton_regreso_info)
+
     }
 }
